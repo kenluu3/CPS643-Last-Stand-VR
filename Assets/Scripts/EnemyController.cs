52a556427e45d69ba4f8306339eb279feb46d566
dannykhuu2 <dannykhuu2@gmail.com>
2023-03-24 19:11:18 -0400

    modify enemy script behaviours and overall improvements

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,76 +9,33 @@ public class EnemyController : MonoBehaviour
     public Transform player;
     public LayerMask groundLayer, playerLayer;
     public GameObject projectile;
+    public Transform cannonTransform;
     public float health;
 
-    //patroling, might not need
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
-
-    //attatcking
+    //attacking
     public float attackCooldown;
     bool attacked;
 
-    //states
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
+    public float attackRange;
+    public bool playerInAttackRange;
 
     private void Awake()
-    {   
-        player = GameObject.Find("Camera").transform;
+    {
         agent = GetComponent<NavMeshAgent>();
     }
     void Update()
     {
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
-        if (!playerInSightRange && !playerInAttackRange)
-        {
-            Patroling();
-        }
-        if (playerInSightRange && !playerInAttackRange)
+        if (!playerInAttackRange)
         {
             Chase();
         }
-        if (playerInSightRange && playerInAttackRange)
+        else
         {
             Attack();
         }
     }
 
-    private void Patroling()
-    {
-        if (!walkPointSet)
-        {
-            FindWalkPoint();
-        }
-        if(walkPointSet)
-        {
-            agent.SetDestination(walkPoint);
-        }
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        if (distanceToWalkPoint.magnitude < 1f)
-        {
-            walkPointSet = false;
-        }
-    }
-
-    private void FindWalkPoint()
-    {
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer))
-        {
-            walkPointSet = true;
-        }
-    }
-    
     private void Chase()
     {
         agent.SetDestination(player.position);
@@ -89,14 +46,19 @@ public class EnemyController : MonoBehaviour
         agent.SetDestination(transform.position);
         transform.LookAt(player);
 
-        Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-        rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-
         if (!attacked)
         {
+
+            Rigidbody rb = Instantiate(projectile, cannonTransform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            Vector3 direction = player.position - cannonTransform.position;
+            direction += Vector3.up;
+            rb.AddForce(direction.normalized * 32f, ForceMode.Impulse);
+
             attacked = true;
             Invoke(nameof(ResetAttack), attackCooldown);
+
+            // check if this projetile hits the player
+            Destroy(rb.gameObject, 3f);
         }
     }
 
@@ -124,7 +86,5 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
