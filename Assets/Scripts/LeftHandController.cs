724b619f5e5ef83067c1bd86f7c2e2aa4d1cd5c3
kenluu3 <ken.luuu@hotmail.com>
2023-03-31 14:33:17 -0400

    teleport

--- Assets/Scripts/LeftHandController.cs
+++ Assets/Scripts/LeftHandController.cs
@@ -6,16 +6,26 @@ using Valve.VR;
 public class LeftHandController : HandController
 {
     public SteamVR_Action_Boolean fireGun;
+    public SteamVR_Action_Boolean teleportPlayer;
     public SteamVR_Action_Vector2 movePlayer;
 
     public Transform playerRig;
     public Transform playerCamera;
     private float moveSpeed = 5.0f;
 
+    private float elapsedTimeTeleport = 5.0f;
+    private float teleportCooldown = 1.0f;
+
     void Start()
     {
         heldWeapon = transform.Find("Gun").gameObject;
         fireGun.AddOnStateDownListener(OnFire, controller);
+        teleportPlayer.AddOnStateDownListener(OnTeleport, controller);
+    }
+    
+    void Update()
+    {
+        elapsedTimeTeleport += Time.deltaTime;
     }
 
     void FixedUpdate()
@@ -24,9 +34,33 @@ public class LeftHandController : HandController
         playerRig.position += Vector3.ProjectOnPlane(Time.deltaTime * movementDir * moveSpeed, Vector3.up);
     }
 
+    void OnTeleport(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Hello World");
+        if (elapsedTimeTeleport >= teleportCooldown)
+        {
+            StartCoroutine(TeleportPlayer());
+        }
+    }
+
     void OnFire(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
         heldWeapon.GetComponent<GunController>().FireBullet();
         TriggerHaptics();
+    }
+
+    IEnumerator TeleportPlayer()
+    {
+        SteamVR_Fade.View(Color.black, .75f);
+        Vector3 movementDir = playerCamera.TransformDirection(new Vector3(movePlayer.axis.x, 0, movePlayer.axis.y));
+        Vector3 target = playerRig.position + Vector3.ProjectOnPlane(movementDir * moveSpeed * 2.0f, Vector3.up);
+        Vector3 offset = playerRig.position - playerCamera.position;
+        offset.y = 0;
+
+        yield return new WaitForSeconds(.75f);
+        playerRig.position = target + offset;
+
+        SteamVR_Fade.View(Color.clear, .75f);
+        elapsedTimeTeleport = 0.0f;
     }
 }
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -129,6 +129,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean PlayerTeleport
+        {
+            get
+            {
+                return SteamVR_Actions.default_PlayerTeleport;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
