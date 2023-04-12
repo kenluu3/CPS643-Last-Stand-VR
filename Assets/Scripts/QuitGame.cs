06434e833cf2a22fb08fca6d3c586aeb059b0dda
kenluu3 <ken.luuu@hotmail.com>
2023-04-12 13:42:50 -0400

    updating quit script

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -6,10 +6,14 @@ public class QuitGame : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
         /* Bullet layer */
         if (collision.gameObject.layer == 8)
         {
             Application.Quit();
         }
+        #endif
     }
 }
